640db96678a29303c94f3d6295bd0ce17bedef63
mohammadAinia <mmyyttt@gmail.com>
2025-02-03 15:26:07 +0300

    up

--- backend/index.js
+++ backend/index.js
@@ -651,7 +651,7 @@ const sendButtons = async (to, text, buttons) => {
 
     try {
         await axios.post(
-            `${process.env.WHATSAPP_API_URL}/${process.env.PHONE_NUMBER_ID}/messages`,
+            `${process.env.WHATSAPP_API_URL}/${process.env.PHONE_NUMBER_ID}`,
             payload,
             {
                 headers: {
