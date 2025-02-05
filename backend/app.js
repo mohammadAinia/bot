85901ba0c010d509bd35882b682321e3ef38d087
mohammadAinia <mmyyttt@gmail.com>
2025-02-06 01:00:16 +0300

    upp

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(bodyParser.json());
 
+
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+});
+
 app.use('/admin', adminRoutes);
 app.use('/', webhookRoutes);
 
