3a0dee918ac3e24946957992c06130afec3bc0c8
mohammadAinia <mmyyttt@gmail.com>
2025-02-06 11:52:32 +0300

    UP

--- backend/services/whatsappService.js
+++ backend/services/whatsappService.js
@@ -4,6 +4,25 @@ import { getSystemMessages } from '../models/messageModel.js';
 
 const userSessions = {};
 
+const STATES = {
+    WELCOME: 0,
+    FAQ: "faq",
+    NAME: 1,
+    PHONE_CONFIRM: "phone_confirm",
+    PHONE_INPUT: "phone_input",
+    EMAIL: 3,
+    ADDRESS: 4,
+    CITY: 7,
+    STREET: 9,
+    BUILDING_NAME: 10,
+    FLAT_NO: 11,
+    LATITUDE: 12,
+    LONGITUDE: 13,
+    QUANTITY: 6,
+    CONFIRMATION: 5,
+    MODIFY: "modify"  // New state for modification
+};
+
 export const handleIncomingMessage = async (body) => {
     console.log('Incoming Webhook Data:', body); // Log the incoming data for debugging
 
